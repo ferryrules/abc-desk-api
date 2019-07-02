b7a3f4fcfbb195f3999fd6e682dfb594616fd2d3
ferryrules <ferris@ferryrules.com>
2019-07-02 14:27:37 -0400

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,41 @@ Rails.application.routes.draw do
 
   resources :comp_users
   resources :users
+  resources :companies
+  resources :employees
+  resources :departments
+  resources :recurring_adjustments
   resources :paycheck_adjustments
   resources :paychecks
   resources :payrolls
-  resources :recurring_adjustments
-  resources :departments
-  resources :employees
   resources :tickets
-  resources :companies
+  #
+  # resources :users do
+  #   resources :companies do
+  #     resources :tickets
+  #   end
+  # end
+  #
+  # resources :users do
+  #   resources :companies do
+  #     resources :employees do
+  #       resources :departments
+  #       resources :recurring_adjustments
+  #     end
+  #   end
+  # end
+  #
+  # resources :users do
+  #   resources :companies do
+  #     resources :payrolls do
+  #       resources :paychecks do
+  #         resources :paycheck_adjustments
+  #       end
+  #     end
+  #   end
+  # end
+
+
 
   resources :users, only: %i[create]
   post '/login', to: 'auth#create'
