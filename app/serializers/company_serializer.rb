04954949799b8fad42f416e924a0e9e67d5e89f3
ferryrules <ferris@ferryrules.com>
2019-07-10 17:20:18 -0400

    filter

--- app/serializers/company_serializer.rb
+++ app/serializers/company_serializer.rb
@@ -19,55 +19,55 @@ class CompanySerializer < ActiveModel::Serializer
   end
 
   def terminated
-    Employee.all.each do |emp|
+    Employee.all.filter do |emp|
       emp.active_status == 'Terminated'
     end
   end
 
   def hourly
-    Employee.all.each do |emp|
+    Employee.all.filter do |emp|
       emp.pay_type == 'Hourly'
     end
   end
 
   def salary
-    Employee.all.each do |emp|
+    Employee.all.filter do |emp|
       emp.pay_type == 'Salary'
     end
   end
 
   def high
-    Ticket.all.each do |ticket|
+    Ticket.all.filter do |ticket|
       ticket.priority == 'High'
     end
   end
 
   def medium
-    Ticket.all.each do |ticket|
+    Ticket.all.filter do |ticket|
       ticket.priority == 'Medium'
     end
   end
 
   def low
-    Ticket.all.each do |ticket|
+    Ticket.all.filter do |ticket|
       ticket.priority == 'Low'
     end
   end
 
   def open
-    Ticket.all.each do |ticket|
+    Ticket.all.filter do |ticket|
       ticket.ticket_status == 'Open'
     end
   end
 
   def pending
-    Ticket.all.each do |ticket|
+    Ticket.all.filter do |ticket|
       ticket.ticket_status == 'Pending'
     end
   end
 
   def closed
-    Ticket.all.each do |ticket|
+    Ticket.all.filter do |ticket|
       ticket.ticket_status == 'Closed'
     end
   end
