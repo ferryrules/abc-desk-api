a08b33907558e8891f3b70bbcff40e43111a4858
ferryrules <ferris@ferryrules.com>
2019-08-29 16:00:59 -0400

    [CHANGE]: pay rate to float

--- db/migrate/20190627141823_create_employees.rb
+++ db/migrate/20190627141823_create_employees.rb
@@ -3,7 +3,7 @@ class CreateEmployees < ActiveRecord::Migration[5.2]
     create_table :employees do |t|
       t.string :full_name
       t.string :pay_type
-      t.integer :pay_rate
+      t.float :pay_rate
       t.string :active_status, default: 'Active'
       t.string :filing_status
       t.integer :w4_allowance, default: 0
--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,7 @@ ActiveRecord::Schema.define(version: 2019_08_29_163435) do
   create_table "employees", force: :cascade do |t|
     t.string "full_name"
     t.string "pay_type"
-    t.integer "pay_rate"
+    t.float "pay_rate"
     t.string "active_status", default: "Active"
     t.string "filing_status"
     t.integer "w4_allowance", default: 0
