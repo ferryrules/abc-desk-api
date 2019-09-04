a5bb1f5dd859624c5b4e6a6e77ba8bce4a6bdef3
ferryrules <ferris@ferryrules.com>
2019-09-04 11:22:01 -0400

    [UPDATE]: cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins '*'
+    origins 'https://nifty-edison-ff1c31.netlify.com'
 
     resource '*',
       headers: :any,
