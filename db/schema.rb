a1f8c207c8d5fbaf510998d7cb8fcd79ab83c6d9
ferryrules <ferris@ferryrules.com>
2019-09-10 23:01:47 -0400

    [ADD] ticket updates

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_11_025548) do
+ActiveRecord::Schema.define(version: 2019_09_11_025908) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -105,8 +105,11 @@ ActiveRecord::Schema.define(version: 2019_09_11_025548) do
   end
 
   create_table "updates", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_updates_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -130,4 +133,5 @@ ActiveRecord::Schema.define(version: 2019_09_11_025548) do
   add_foreign_key "recurring_adjustments", "employees"
   add_foreign_key "tickets", "companies"
   add_foreign_key "tickets", "users"
+  add_foreign_key "updates", "users"
 end
